0f1061accec8abfad2bd7c0bfe2b677d43047084
Devclaim <56877180+Devclaim@users.noreply.github.com>
2023-10-03 00:52:48 +0200

    add three js scene

--- javascript/MainScene.js
+++ javascript/MainScene.js
@@ -1,53 +1,70 @@
 console.log('Initiating three.js Scene');
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#bg'),
+  canvas: document.querySelector('#canvas'),
+  alpha: true,
 });
 
+renderer.setClearColor( 0x000000, 0 );
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
-camera.position.setX(-3);
+camera.position.setX(-15);
 
-// Torus
+const loader = new GLTFLoader();
+let plant = null;
 
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
-const torus = new THREE.Mesh(geometry, material);
-
-scene.add(torus);
+loader.load('models/pothos_plant.glb', function ( gltf ) {
+  plant = gltf.scene;
+	scene.add( plant );
+  plant.scale.set(2.5,2.5,2.5);
+  plant.position.setY(2);
+}, undefined, function ( error ) {
+	console.error( error );
+});
 
 // Light
-
-const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(5,5,5);
-
 const ambientLight = new THREE.AmbientLight(0xffffff);
 
-scene.add(pointLight, ambientLight);
+//TODO: CHANGE COLOR DEPENDING ON WARN STATUS
+const directionalLight = new THREE.DirectionalLight( 0xe3fda8, 5 );
+directionalLight.position.setY(10);
+directionalLight.position.setX(-20);
 
-// Helpers
+scene.add(ambientLight, directionalLight);
 
-const lightHelper = new THREE.PointLightHelper(pointLight)
-const gridHelper = new THREE.GridHelper(200, 50);
-scene.add(lightHelper, gridHelper)
+function addStar() {
+  const starGeo = new THREE.SphereGeometry(0.05, 20, 20);
+  const starMat = new THREE.MeshStandardMaterial({ color: 0xffffff });
+  const star = new THREE.Mesh(starGeo, starMat);
+
+  let [x, y, z] = Array(3)
+  .fill()
+  .map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+
+Array(400).fill().forEach(addStar);
 
 const controls = new OrbitControls(camera, renderer.domElement);
+controls.enabled = false;
 
 function animate() {
     requestAnimationFrame(animate);
-  
-    torus.rotation.x += 0.01;
-    torus.rotation.y += 0.005;
-    torus.rotation.z += 0.01;
-    
-    controls.update();
+
+    if(plant) {
+      plant.rotation.y += 0.003;
+    }
+    scene.rotateOnAxis(new THREE.Vector3(0,1,0), -0.002);
+
     renderer.render(scene, camera);
   }
   
